e9c417f845ff2a3454c9bd743af021a18bc4b05d
cnnbless <antongolubchuk@gmail.com>
2024-03-06 12:23:03 +0200

    Add InEarHeadphones class

---
+++ InEarHeadphones.h
@@ -0,0 +1,14 @@
+//
+// Created by Tony on 06.03.2024.
+//
+
+#ifndef DEVICES_INEARHEADPHONES_H
+#define DEVICES_INEARHEADPHONES_H
+
+
+class InEarHeadphones {
+
+};
+
+
+#endif //DEVICES_INEARHEADPHONES_H
