c0f4d4e66e205a99715a75b7d551b5748a2dc6a4
cnnbless <antongolubchuk@gmail.com>
2024-03-06 12:18:54 +0200

    Add MechanicKeyboard class

---
+++ MechanicKeyboard.h
@@ -0,0 +1,14 @@
+//
+// Created by Tony on 06.03.2024.
+//
+
+#ifndef DEVICES_MECHANICKEYBOARD_H
+#define DEVICES_MECHANICKEYBOARD_H
+
+
+class MechanicKeyboard {
+
+};
+
+
+#endif //DEVICES_MECHANICKEYBOARD_H
