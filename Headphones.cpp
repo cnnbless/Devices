66f029c873a4f81c1b08e18e1fdfcf4cf841d059
cnnbless <antongolubchuk@gmail.com>
2024-03-06 12:09:55 +0200

    Add constructor, add move constructor

--- Headphones.cpp
+++ Headphones.cpp
@@ -3,3 +3,62 @@
 //
 
 #include "Headphones.h"
+
+#include <iostream>
+using namespace std;
+string Headphones::set_name(string name)
+{
+    return this->name = name;
+}
+string Headphones::get_name()
+{
+    return name;
+}
+float Headphones::set_weight(float weight)
+{
+    return this->weight=weight;
+}
+float Headphones::get_weight()
+{
+    return weight;
+}
+string Headphones::set_TypeOfMaterial(string type_of_material)
+{
+    return this->type_of_material=type_of_material;
+}
+string Headphones::get_TypeOfMaterial()
+{
+    return type_of_material;
+}
+bool Headphones::set_microphone(bool microphone)
+{
+    return this->microphone=microphone;
+}
+bool Headphones::get_microphone()
+{
+    return microphone;
+}
+void Headphones::info()
+{
+    cout<<"INFO: "<<endl
+        <<"Назва наушників: "<<name
+        <<"\nВага наушників: "<<weight<<" кг"
+        <<"\nТип наушників наушників: "<<type_of_material
+        <<"\nНаушники з мікрофоном: "<<microphone<<endl;
+}
+
+Headphones::Headphones(std::string &&NameOfKeyboard, float Weight, std::string &&TypeOfMaterial, bool MICROPHONE):
+        name{NameOfKeyboard}, weight(Weight), type_of_material(TypeOfMaterial), microphone(MICROPHONE)
+{
+    cout<<"called Headphones constructor"<<endl;
+}
+Headphones::Headphones(const Headphones &&other) noexcept{
+    name=move(other.name);
+    weight=other.weight;
+    type_of_material=move(other.type_of_material);
+    microphone=other.microphone;
+    cout<<"Headphones move constructor"<<endl;
+}
+Headphones::~Headphones(){
+    cout<<"called Headphones destructor"<<endl;
+}
--- Headphones.h
+++ Headphones.h
@@ -2,13 +2,36 @@
 // Created by Tony on 06.03.2024.
 //
 
-#ifndef DEVICES_HEADPHONES_H
-#define DEVICES_HEADPHONES_H
+#ifndef DEVICESOOP_HEADPHONES_H
+#define DEVICESOOP_HEADPHONES_H
 
 
+#include <iostream>
+
+using namespace std;
 class Headphones {
+private:
+    string name;
+    float weight;
+    string type_of_material;
+    bool microphone;
 
+public:
+    string set_name(string name);
+    string get_name();
+    float set_weight(float weight);
+    float get_weight();
+    string set_TypeOfMaterial(string type_of_material);
+    string get_TypeOfMaterial();
+    bool set_microphone(bool microphone);
+    bool get_microphone();
+    void info();
+
+    Headphones(string &&NameOfHeadphones="None", float Weight=0, string &&TypeOfMaterial="None", bool MICROPHONE=false);
+    Headphones(const Headphones &&other)noexcept;
+    ~Headphones();
 };
 
 
-#endif //DEVICES_HEADPHONES_H
+
+#endif //DEVICESOOP_HEADPHONES_H
