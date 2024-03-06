e058d6fd3457cecdb11ecebaa8ac507481ba8b0e
cnnbless <antongolubchuk@gmail.com>
2024-03-06 12:30:41 +0200

    Add derived MechanicKeyaboard objects, derived copy constructor

--- MechanicKeyboard.h
+++ MechanicKeyboard.h
@@ -2,13 +2,26 @@
 // Created by Tony on 06.03.2024.
 //
 
-#ifndef DEVICES_MECHANICKEYBOARD_H
-#define DEVICES_MECHANICKEYBOARD_H
+#ifndef DEVICESOOP_MECHANICKEYBOARD_H
+#define DEVICESOOP_MECHANICKEYBOARD_H
+#include "Keyboard.h"
+#include <iostream>
 
+using namespace std;
 
-class MechanicKeyboard {
+class MechanicKeyboard: public Keyboard{
+private:
+    string colorOfKeyboard;
+public:
+    string set_colorOfKeyboard(string &&colorOfKeyboard);
+    string get_colorOfKeyboard();
+    void info();
+
+    MechanicKeyboard(string name="None",float weight=0, string typeOfMaterial="None",string &&colorOfKeyboard="None");
+    MechanicKeyboard(const MechanicKeyboard &other);
+    ~MechanicKeyboard();
 
 };
 
 
-#endif //DEVICES_MECHANICKEYBOARD_H
+#endif //DEVICESOOP_MECHANICKE
