4b8bf8e32d8a271b62e767298e9f6786ea13ed5b
cnnbless <antongolubchuk@gmail.com>
2024-03-06 12:06:39 +0200

    Add constructor, add friend operators(>>,<<), add unary and binary operators

--- Mouse.cpp
+++ Mouse.cpp
@@ -1,5 +1,145 @@
 //
-// Created by Tony on 06.03.2024.
+// Created by Endi on 27.02.2024.
 //
 
 #include "Mouse.h"
+#include <iostream>
+int Mouse::AmountOfMice=0;
+
+string Mouse::set_name(string name)
+{
+
+    return this->name=name;
+}
+string Mouse::get_name()
+{
+    return name;
+}
+float Mouse::set_weight(float weight)
+{
+    return this->weight=weight;
+}
+float Mouse::get_weight()
+{
+    return weight;
+}
+string Mouse::set_TypeOfMaterial(string typeOfMaterial)
+{
+    return type_of_material=typeOfMaterial;
+}
+string Mouse::get_TypeOfMaterial()
+{
+    return type_of_material;
+}
+bool Mouse::set_wireless(bool wireless)
+{
+    return this->wireless=wireless;
+}
+bool Mouse::get_wireless()
+{
+    return wireless;
+}
+void Mouse::info()
+{
+    cout<<"INFO: "<<endl
+        <<"Назва мишки: "<<name
+        <<"\nВага мишки: "<<weight<<" кг"
+        <<"\nТип матеріалу мишки: "<<type_of_material
+        <<"\nМишка провідна: "<<wireless<<endl;
+}
+
+int Mouse::get_amountOfMice() {
+    return AmountOfMice;
+}
+
+std::ostream &operator<<(std::ostream &os, const Mouse &mouse) {
+    os << "Назва мишки: " <<mouse.name  << endl;
+    os << "Вага мишки: " <<mouse.weight  << endl;
+    os << "Тип матеріалу мишки: "<< mouse.type_of_material<<endl;
+    os << "Провідна: "<<mouse.wireless<<endl;
+    return os;
+}
+std::istream &operator>>(std::istream &is, Mouse &mouse) {
+    cout<<"Введіть назву мишки: ";
+    is >> mouse.name;
+    cout<<"Введіть вагу мишки: ";
+    is >> mouse.weight;
+    cout<<"Введіть тип матеріалу мишки: ";
+    is >> mouse.type_of_material;
+    cout<<"Провідна true/false: ";
+    is >> mouse.wireless;
+    return is;
+}
+Mouse &Mouse::operator=(const Mouse &other){
+    if(this!=&other){
+        name=other.name;
+        weight=other.weight;
+        type_of_material=other.type_of_material;
+        wireless=other.wireless;
+    }
+    return  *this;
+}
+Mouse Mouse::operator+(float num) {
+    weight+=num;
+    return *this;
+}
+Mouse Mouse::operator-(float num) {
+    if(weight<0.1){
+        weight=0;
+        return *this;
+    }
+    weight-=num;
+    return *this;
+}
+Mouse Mouse::operator+=(float num) {
+    weight+=num;
+    return *this;
+}
+Mouse Mouse::operator-=(float num) {
+    if(weight<0.1){
+        weight=0;
+        return *this;
+    }
+    weight-=num;
+    return *this;
+}
+Mouse Mouse::operator--(){
+    if(weight<0.1){
+        weight=0;
+        return *this;
+    }
+    weight-=0.100;
+    return *this;
+}
+Mouse Mouse::operator++() {
+    weight+=0.100;
+    return *this;
+}
+Mouse::Mouse()
+{
+    AmountOfMice++;
+    name="None";
+    weight=0;
+    type_of_material="None";
+    wireless= false;
+    cout<<"called Mouse constructor"<<endl;
+}
+Mouse::Mouse(std::string NameOfMouse): Mouse()
+{
+    name=NameOfMouse;
+
+}
+Mouse::Mouse(std::string NameOfMouse, float Weight):Mouse(NameOfMouse)
+{
+    weight=Weight;
+}
+Mouse::Mouse(std::string NameOfMouse, float Weight, std::string NameTypeOfMaterial):Mouse(NameOfMouse,  Weight) {
+    type_of_material=NameTypeOfMaterial;
+}
+Mouse::Mouse(std::string NameOfMouse, float Weight, std::string NameTypeOfMaterial, bool Wireless):Mouse(NameOfMouse,  Weight,  NameTypeOfMaterial) {
+    wireless=Wireless;
+}
+Mouse::~Mouse() {
+    AmountOfMice--;
+    cout<<"called Mouse destructor"<<endl;
+}
--- Mouse.h
+++ Mouse.h
@@ -1,14 +1,54 @@
 //
-// Created by Tony on 06.03.2024.
+// Created by Endi on 27.02.2024.
 //
 
-#ifndef DEVICES_MOUSE_H
-#define DEVICES_MOUSE_H
+#ifndef DEVICESOOP_MOUSE_H
+#define DEVICESOOP_MOUSE_H
 
 
+#include <iostream>
+
+using namespace std;
 class Mouse {
 
+private:
+    string name;
+    float weight;
+    string type_of_material;
+    bool wireless;
+    static int AmountOfMice;
+
+public:
+    string set_name(string name);
+    string get_name();
+    float set_weight(float weight);
+    float get_weight();
+    string set_TypeOfMaterial(string typeOfMaterial);
+    string get_TypeOfMaterial();
+    bool set_wireless(bool wireless);
+    bool get_wireless();
+    void info();
+    int get_amountOfMice();
+
+    friend std::ostream  &operator<<(std::ostream &os, const Mouse &mouse);
+    friend std::istream &operator>>(std::istream &is, Mouse &mouse);
+
+    Mouse &operator=(const Mouse &other);
+    Mouse operator+(float num);
+    Mouse operator-(float num);
+    Mouse operator+=(float num);
+    Mouse operator-=(float num);
+
+    Mouse operator--();
+    Mouse operator++();
+
+    Mouse();
+    Mouse(string NameOfMouse);
+    Mouse(string NameOfMouse, float Weight);
+    Mouse(string NameOfMouse, float Weight, string TypeOfMaterial);
+    Mouse(string NameOfMouse, float Weight, string TypeOfMaterial, bool Wireless);
+    ~Mouse();
 };
 
 
-#endif //DEVICES_MOUSE_H
+#endif //DEVICESOOP_MOUSE_H
