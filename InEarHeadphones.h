09b74758fbb2cad87fc7c94e22c7cc2eceea85d9
cnnbless <antongolubchuk@gmail.com>
2024-03-06 12:33:55 +0200

    Add derived InEarHeadphones objects, add move constructor

--- InEarHeadphones.h
+++ InEarHeadphones.h
@@ -2,13 +2,27 @@
 // Created by Tony on 06.03.2024.
 //
 
-#ifndef DEVICES_INEARHEADPHONES_H
-#define DEVICES_INEARHEADPHONES_H
 
+#ifndef DEVICESOOP_INEARHEADPHONES_H
+#define DEVICESOOP_INEARHEADPHONES_H
 
-class InEarHeadphones {
+#include "Headphones.h"
+#include <iostream>
+
+using namespace std;
+
+class InEarHeadphones: public Headphones{
+private:
+    bool wired;
+public:
+    bool set_wired(bool wired);
+    bool get_wired();
+    void info();
+    InEarHeadphones(string name="None",float weight=0, string typeOfMaterial="None",bool microphone=false,bool wired= false);
+    InEarHeadphones(InEarHeadphones&& other)noexcept;
+    ~InEarHeadphones();
 
 };
 
 
-#endif //DEVICES_INEARHEADPHONES_H
+#endif //DEVICESOOP_INEARHEADPHONES_H
