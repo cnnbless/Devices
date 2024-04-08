93bfc02d60265a5e36a23ca49ef8fa96ce5342ac
cnnbless <antongolubchuk@gmail.com>
2024-04-08 17:09:15 +0300

    Added derived functions: writeProduct, print,readData

--- MechanicKeyboard.cpp
+++ MechanicKeyboard.cpp
@@ -1,45 +1,57 @@
 //
-// Created by Tony on 06.03.2024.
+// Created by Endi on 29.02.2024.
 //
-
 
 #include "MechanicKeyboard.h"
 #include "Keyboard.h"
-
+#include <fstream>
 using namespace std;
 
-string MechanicKeyboard::set_colorOfKeyboard(std::string &&colorOfKeyboard) {
+string MechanicKeyboard::set_colorOfKeyboard(string &&colorOfKeyboard) {
     return this->colorOfKeyboard=colorOfKeyboard;
 }
-string MechanicKeyboard::get_colorOfKeyboard() {
+string MechanicKeyboard::get_colorOfKeyboard()const {
     return colorOfKeyboard;
 }
-void MechanicKeyboard::info() {
-    Keyboard::info();
-    cout<<"Колір клавіатури: "<<colorOfKeyboard<<endl;
-}
 
-void MechanicKeyboard::doSomething() const{
-    cout<<"MechanicKeyboard"<<endl;
+void MechanicKeyboard::writeProduct() {
+    Keyboard::writeProduct();
+    ofstream file("C:\\Users\\Tony\\CLionProjects\\Devices\\files\\Products.txt", ios::app);
+    string data=","+colorOfKeyboard;
+    file<<data;
+    file.close();
 }
 
 void MechanicKeyboard::print(std::ostream &os) const {
-    Keyboard::print(os);
-    os<<"Колір клавіатури: "<<colorOfKeyboard<<endl;
+    os<<endl<<"---Товар: Механічна клавіатура---"<<endl
+      <<"Номер товару: "<<Keyboard::get_product_number()<<endl
+      <<"Назва: "<<Keyboard::get_name()<<endl
+      <<"Вага: "<<Keyboard::get_weight()<<endl
+      <<"Матеріал: "<<Keyboard::get_typeOfMaterial()<<endl
+      <<"Колір: "<<colorOfKeyboard<<endl
+      <<"Ціна: "<<Keyboard::get_price()<<endl;
 }
 
-void MechanicKeyboard::print_class_name() const {
-    cout<<"Class name: MechanicKeyboard"<<endl;
+void MechanicKeyboard::readData(std::istream &is) {
+    Keyboard::readData(is);
+    cout<<"Введіть колір клавіатури: ";
+    is >> colorOfKeyboard;
 }
 
-MechanicKeyboard::MechanicKeyboard(string name, float weight, string typeOfMaterial,string &&colorOfKeyboard):
-        Keyboard{string{name},float{weight},string{typeOfMaterial}}{
-    this->colorOfKeyboard=colorOfKeyboard;
-    cout<<"MechanicKeyboard constructor"<<endl;
-}
-MechanicKeyboard::MechanicKeyboard(const MechanicKeyboard &other):Keyboard(other), colorOfKeyboard(other.colorOfKeyboard){
+MechanicKeyboard::MechanicKeyboard(const MechanicKeyboard &other):Keyboard(other)
+        , colorOfKeyboard(other.colorOfKeyboard)
+{
     cout<<"MechanicKeyboard copy"<<endl;
 }
+
+MechanicKeyboard::MechanicKeyboard(int product_number, string &&name, float weight, string &&type_of_material,
+                                   float price, string &&colorOfKeyboard):
+        Keyboard{int{product_number},string{name},float{weight}
+                ,string{type_of_material},float{price}}
+{
+    this->colorOfKeyboard=colorOfKeyboard;
+    //cout<<"called MechanicKeyboard constructor"<<endl;
+}
 MechanicKeyboard::~MechanicKeyboard() {
-    cout<<"called MechanicKeyboard destructor"<<endl;
+    //cout<<"called MechanicKeyboard destructor"<<endl;
 }
--- MechanicKeyboard.h
+++ MechanicKeyboard.h
@@ -1,12 +1,12 @@
 //
-// Created by Tony on 06.03.2024.
+// Created by Endi on 29.02.2024.
 //
 
 #ifndef DEVICESOOP_MECHANICKEYBOARD_H
 #define DEVICESOOP_MECHANICKEYBOARD_H
+
 #include "Keyboard.h"
 #include <iostream>
-
 using namespace std;
 
 class MechanicKeyboard: public Keyboard{
@@ -14,19 +14,16 @@ private:
     string colorOfKeyboard;
 public:
     string set_colorOfKeyboard(string &&colorOfKeyboard);
-    string get_colorOfKeyboard();
-    void info();
-
-    void doSomething()const;
+    string get_colorOfKeyboard()const;
 
     void print(std::ostream &os) const override;
-    void print_class_name()const override;
-
-    MechanicKeyboard(string name="None",float weight=0, string typeOfMaterial="None",string &&colorOfKeyboard="None");
+    void readData(std::istream& is) override;
+    void writeProduct() override;
     MechanicKeyboard(const MechanicKeyboard &other);
+
+    MechanicKeyboard(int product_number=0,string &&name="None", float weight=0, string &&type_of_material="None"
+            ,float price=0,string &&colorOfKeyboard="None");
     ~MechanicKeyboard() override;
 
 };
-
-
 #endif //DEVICESOOP_MECHANICKEYBOARD_H
