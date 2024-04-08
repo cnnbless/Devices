336a840f758fbebdc1433c76b07c1a5ca3e8f209
cnnbless <antongolubchuk@gmail.com>
2024-04-08 16:57:22 +0300

    Added main body of the program, added main file URL`s

--- main.cpp
+++ main.cpp
@@ -1,55 +1,152 @@
 #include <iostream>
-#include "Mouse.h"
-#include "Keyboard.h"
-#include "Headphones.h"
-#include "MechanicKeyboard.h"
-#include "InEarHeadphones.h"
-#include "GamingMouse.h"
+#include <fstream>
+#include <string>
+#include "vector"
+#include "User.h"
 #include "Printable.h"
+#include "Admin.h"
+#include <memory>
+
 using namespace std;
-
-
 ostream &operator<<(ostream &os, const Printable &obj) {
     obj.print(os);
     return os;
 }
+istream &operator>>(std::istream &is, Printable &obj) {
+    obj.readData(is);
+    return is;
+}
 
 int main() {
-//проблему Static Method Binding
-//    Keyboard a;
-//    MechanicKeyboard b;
-//    Keyboard *p=&b;
-//    p->doSomething();
+    string URL_login = "C:\\Users\\Tony\\CLionProjects\\Devices\\files\\Login.txt";
+    string URL_product = "C:\\Users\\Tony\\CLionProjects\\Devices\\files\\Products.txt";
 
-// (Run-time) поліморфізму
-//    Mouse a;
-//    GamingMouse b;
-//    Mouse *p=&b;
-//    p->info();
+    unique_ptr<ifstream> file(new ifstream(URL_login));
+    vector<string> logins;
+    string line;
+    while (getline(*file, line)) {
+        if (!line.empty()) {
+            logins.push_back(line);
+        }
+    }
+    file->close();
 
-// virtual destructor
-//    Keyboard *a = new MechanicKeyboard;
-//    delete a;
+    string login;
+    bool user_flag = false;
+    bool admin_flag = false;
 
-//final
-//    GamingMouse a;
-//    a.Click();
+    do {
+        cout << "Введіть ваш логін: ";
+        cin >> login;
 
-// Base class reference
-//    InEarHeadphones a;
-//    Headphones b;
-//    InEarHeadphones &pos = a;
-//    pos.print_name_class();
-// Pure virtual function + інтерфейс
-//    MechanicKeyboard a("rty",2.4,"metal","green");
-//    cout << a;
-//    GamingMouse b("wirma",1.3,"plastic",true,3,true);
-//    cout<<b;
-//    InEarHeadphones c("warmila",4,"plastic",true,false);
-//    cout<<c;
+        if (login == "admin") {
+            cout << "Введіть пароль: ";
+            string password;
+            cin >> password;
+            if (password == "admin") {
+                admin_flag = true;
+                break;
+            } else {
+                cout << "Ви ввели невірний пароль!" << endl << endl;
+            }
+        } else {
+            bool old_user = false;
+            for (const string& existing_login : logins) {
+                if (login == existing_login) {
+                    user_flag = true;
+                    old_user = true;
+                    cout << "OLD USER" << endl;
+                    break;
+                }
+            }
+            if (!old_user) {
+                user_flag = true;
+                logins.push_back(login);
 
-// 7
-//    MechanicKeyboard a;
-//    a.print_class_name();
+                unique_ptr<ofstream> file(new ofstream(URL_login, ios::app));
+                *file << endl << login;
+                file->close();
+
+                cout << "NEW USER" << endl;
+
+                string purchase_name_file = "C:\\Users\\Tony\\CLionProjects\\Devices\\files\\" + login + "_purchase.txt";
+                unique_ptr<ofstream> purchase_history(new ofstream(purchase_name_file));
+                purchase_history->close();
+            }
+        }
+    } while (!user_flag && !admin_flag);
+
+    std::unique_ptr<Admin> admin;
+    std::unique_ptr<User> user;
+    if (admin_flag) {
+        admin=make_unique<Admin>(URL_product);
+        while (true) {
+            int choice = admin->Admin_Menu();
+            if (choice == 1) {
+                admin->Display_products();
+            }
+            if (choice == 2) {
+                try{
+                    admin->Add_product();
+                }
+                catch (int a){
+                    continue;
+                }
+            }
+            if(choice==3){
+                try{
+                    admin->Product_delete();
+                    remove(admin->get_URL().c_str());
+                    rename("C:\\Users\\Tony\\CLionProjects\\Devices\\files\\temp.txt"
+                            , admin->get_URL().c_str());
+                }
+                catch (int a){
+                    continue;
+                }
+            }
+            if (choice == 4) {
+                break;
+            }
+        }
+    }
+
+    if(user_flag){
+        user = make_unique<User>(login, URL_product);
+        while(true){
+            int choice = user->User_Menu();
+            if(choice==1){
+                user->Display_products();
+            }
+            if(choice==2){
+                try{
+                    user->Add_product_to_Cart();
+                }
+                catch (int a){
+                    continue;}
+            }
+            if(choice==3){
+                try{
+                    user->Delete_from_Cart();
+                    remove(user->get_URL_Cart().c_str());
+                    rename("C:\\Users\\Tony\\CLionProjects\\Devices\\files\\temp.txt"
+                            , user->get_URL_Cart().c_str());
+                }
+                catch(int a){
+                    continue;
+                }
+            }
+            if(choice==4){
+                user->Display_Cart();
+            }
+            if(choice==5){
+                user->Display_history_of_purchases();
+            }
+            if(choice==6){
+                user->Pay();
+                break;
+            }
+        }
+    }
+
     return 0;
 }
