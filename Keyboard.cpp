0388f78886c145576cf6f32f29ea6be30cd77ee2
cnnbless <antongolubchuk@gmail.com>
2024-04-08 17:06:21 +0300

    Added price field, added functions: print, readData; changed writeProduct function

--- Keyboard.cpp
+++ Keyboard.cpp
@@ -1,7 +1,20 @@
+//
+// Created by Endi on 27.02.2024.
+//
+
 #include "Keyboard.h"
 #include "Printable.h"
 #include <iostream>
+#include <fstream>
+#include <limits>
+using namespace std;
 
+int Keyboard::set_product_number(int product_number) {
+    return this->product_number=product_number;
+}
+int Keyboard::get_product_number() const {
+    return product_number;
+}
 string Keyboard::set_name(string name)
 {
     return this->name=name;
@@ -9,15 +22,6 @@ string Keyboard::set_name(string name)
 string Keyboard::get_name() const
 {
     return name;
-}
-string Keyboard::set_TypeOfMaterial(string typeOfMaterial)
-{
-
-    return type_of_material=typeOfMaterial;
-}
-string Keyboard::get_TypeOfMaterial() const
-{
-    return type_of_material;
 }
 float Keyboard::set_weight(float weight)
 {
@@ -27,65 +31,101 @@ float Keyboard::get_weight() const
 {
     return weight;
 }
-void Keyboard::info() const
+string Keyboard::set_typeOfMaterial(string type_of_material)
 {
-    cout<<"INFO: "<<endl
-        <<"Назва клавіатури: "<<name
-        <<"\nВага клавіатури: "<<weight<<" кг"
-        <<"\nТип матеріалу клавіатури: "<<type_of_material<<endl;
+
+    return this->type_of_material=type_of_material;
+}
+string Keyboard::get_typeOfMaterial() const
+{
+    return type_of_material;
+}
+float Keyboard::set_price(float price) {
+    return this->price=price;
+}
+float Keyboard::get_price() const {
+    return price;
 }
 
-void Keyboard::doSomething() const
-{
-    cout<<"Keyboard"<<endl;
+void Keyboard::writeProduct() {
+    ofstream file("C:\\Users\\Tony\\CLionProjects\\Devices\\files\\Products.txt", ios::app);
+    string data=to_string(product_number)+","+name+","+ to_string(weight)+","+type_of_material+","+to_string(price);
+    file<<data;
+    file.close();
 }
+
 
 void Keyboard::print(std::ostream &os) const {
-    os<<"INFO: "<<endl
-      <<"Назва клавіатури: "<<name
-      <<"\nВага клавіатури: "<<weight<<" кг"
-      <<"\nТип матеріалу клавіатури: "<<type_of_material<<endl;
+    os<<endl<<"---Товар: Клавіатура---"<<endl
+      <<"Номер товару: "<<product_number<<endl
+      <<"Назва: "<<name<<endl
+      <<"Вага: "<<weight<<endl
+      <<"Матеріал: "<<type_of_material<<endl
+      <<"Ціна: "<<price<<endl;
 }
 
-void Keyboard::print_class_name() const {
-    cout<<"Class name: Keyboard"<<endl;
+void Keyboard::readData(std::istream &is) {
+    while (true) {
+        cout << "Введіть номер товару: ";
+        if (!(is >> product_number)) {
+            cout << "Некоректний тип даних. Будь ласка, введіть ціле число." << endl;
+            is.clear();
+            is.ignore(numeric_limits<streamsize>::max(), '\n');
+            continue;
+        }
+
+        cout << "Введіть назву клавіатури: ";
+        if (!(is >> name)) {
+            cout << "Некоректний тип даних. Будь ласка, введіть рядок." << endl;
+            is.clear();
+            is.ignore(numeric_limits<streamsize>::max(), '\n');
+            continue;
+        }
+
+        cout << "Введіть вагу клавіатури: ";
+        if (!(is >> weight)) {
+            cout << "Некоректний тип даних. Будь ласка, введіть число." << endl;
+            is.clear();
+            is.ignore(numeric_limits<streamsize>::max(), '\n');
+            continue;
+        }
+
+        cout << "Введіть матеріал клавіатури: ";
+        if (!(is >> type_of_material)) {
+            cout << "Некоректний тип даних. Будь ласка, введіть рядок." << endl;
+            is.clear();
+            is.ignore(numeric_limits<streamsize>::max(), '\n');
+            continue;
+        }
+
+        cout << "Введіть ціну клавіатури: ";
+        if (!(is >> price)) {
+            cout << "Некоректний тип даних. Будь ласка, введіть число." << endl;
+            is.clear();
+            is.ignore(numeric_limits<streamsize>::max(), '\n');
+            continue;
+        }
+
+        break; // Виходимо з циклу, якщо всі дані введено правильно
+    }
 }
 
-Keyboard::Keyboard()
-{
-    name="None";
-    weight=0;
-    type_of_material="None";
-    cout<<"called Keyboard constructor"<<endl;
-}
-Keyboard::Keyboard(std::string &&name)
-{
-    this->name=name;
-    weight=0;
-    type_of_material="None";
-    cout<<"called Keyboard constructor"<<endl;
-}
-Keyboard::Keyboard(std::string &&name, float weight)
-{
-    this->name=name;
-    this->weight=weight;
-    type_of_material="None";
-    cout<<"called Keyboard constructor"<<endl;
-}
-Keyboard::Keyboard(std::string &&name, float weight, std::string &&type_of_material)
-{
-    this->name=name;
-    this->weight=weight;
-    this->type_of_material=type_of_material;
-    cout<<"called Keyboard constructor"<<endl;
-}
 Keyboard::Keyboard(const Keyboard &other) {
+    product_number=other.product_number;
     name = other.name;
     weight = other.weight;
     type_of_material = other.type_of_material;
+    price = other.price;
     cout<<"called copy constructor"<<endl;
 }
+
+Keyboard::Keyboard(int product_number,string &&name, float weight,string &&type_of_material,float price):
+        product_number{product_number},name{name}, weight{weight}, type_of_material{type_of_material},price{price}
+{
+    //cout<<"called Keyboard constructor"<<endl;
+}
+
 Keyboard::~Keyboard()
 {
-    cout<<"called Keyboard destructor"<<endl;
+    //cout<<"called Keyboard destructor"<<endl;
 }
--- Keyboard.h
+++ Keyboard.h
@@ -1,7 +1,6 @@
 //
-// Created by Tony on 06.03.2024.
+// Created by Endi on 27.02.2024.
 //
-
 
 #ifndef DEVICESOOP_KEYBOARD_H
 #define DEVICESOOP_KEYBOARD_H
@@ -11,29 +10,30 @@
 using namespace std;
 class Keyboard: public Printable {
 private:
+    int product_number;
     string name;
     float weight;
     string type_of_material;
+    float price;
 public:
+    int set_product_number(int product_number);
+    int get_product_number()const;
     string set_name(string name);
-    string get_name()const ;
-    string set_TypeOfMaterial(string typeOfMaterial);
-    string get_TypeOfMaterial()const ;
+    string get_name()const;
     float set_weight(float weight);
-    float get_weight()const ;
-    void info()const;
-
-    void doSomething()const;
+    float get_weight()const;
+    string set_typeOfMaterial(string type_of_material);
+    string get_typeOfMaterial()const;
+    float set_price(float price);
+    float get_price()const;
 
     virtual void print(std::ostream &os) const override;
-    virtual void print_class_name()const override;
-
-    Keyboard();
-    Keyboard(string &&name);
-    Keyboard(string &&name, float weight);
-    Keyboard(string &&name, float weight, string &&type_of_material);
+    virtual void readData(std::istream& is) override;
+    virtual void writeProduct() override;
     Keyboard(const Keyboard &other);
+
+
+    Keyboard(int product_number=0,string &&name="None", float weight=0, string &&type_of_material="None",float price=0);
     virtual ~Keyboard();
 };
-
 #endif //DEVICESOOP_KEYBOARD_H
