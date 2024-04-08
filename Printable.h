4209a231d271353dc595ca23d0df49cfc5dea33c
cnnbless <antongolubchuk@gmail.com>
2024-04-08 17:11:53 +0300

    Added readData and writeProduct functions

--- Printable.h
+++ Printable.h
@@ -1,6 +1,7 @@
 //
-// Created by Tony on 21.03.2024.
+// Created by Endi on 14.03.2024.
 //
+
 #ifndef DEVICESOOP_PRINTABLE_H
 #define DEVICESOOP_PRINTABLE_H
 
@@ -10,14 +11,13 @@ using namespace std;
 
 class Printable {
     friend ostream &operator<<(ostream &os, const Printable &obj);
+    friend std::istream &operator>>(istream &is, Printable &obj);
 
 public:
     virtual void print(ostream &os) const = 0;
+    virtual void readData(std::istream &is) = 0;
+    virtual void writeProduct()=0;
     virtual ~Printable() {};
-    virtual void print_class_name()const=0;
 };
-
-
-
 
 #endif //DEVICESOOP_PRINTABLE_H
