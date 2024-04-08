b3ef6e4301bb6a062838ecad66a2d5c1f5873c97
cnnbless <antongolubchuk@gmail.com>
2024-04-08 17:02:45 +0300

    Added functions: writeProduct, readData; changed print function

--- Headphones.cpp
+++ Headphones.cpp
@@ -1,18 +1,22 @@
 //
-// Created by Tony on 06.03.2024.
-//
-//
 // Created by Endi on 27.02.2024.
 //
 
 #include "Headphones.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
+int Headphones::set_product_number(int product_number) {
+    return this->product_number=product_number;
+}
+int Headphones::get_product_number() const {
+    return product_number;
+}
 string Headphones::set_name(string name)
 {
     return this->name = name;
 }
-string Headphones::get_name()
+string Headphones::get_name()const
 {
     return name;
 }
@@ -20,7 +24,7 @@ float Headphones::set_weight(float weight)
 {
     return this->weight=weight;
 }
-float Headphones::get_weight()
+float Headphones::get_weight()const
 {
     return weight;
 }
@@ -28,7 +32,7 @@ string Headphones::set_TypeOfMaterial(string type_of_material)
 {
     return this->type_of_material=type_of_material;
 }
-string Headphones::get_TypeOfMaterial()
+string Headphones::get_TypeOfMaterial()const
 {
     return type_of_material;
 }
@@ -36,48 +40,74 @@ bool Headphones::set_microphone(bool microphone)
 {
     return this->microphone=microphone;
 }
-bool Headphones::get_microphone()
+bool Headphones::get_microphone()const
 {
     return microphone;
 }
-void Headphones::info()
-{
-    cout<<"INFO: "<<endl
-        <<"Назва наушників: "<<name
-        <<"\nВага наушників: "<<weight<<" кг"
-        <<"\nТип наушників наушників: "<<type_of_material
-        <<"\nНаушники з мікрофоном: "<<microphone<<endl;
+float Headphones::set_price(float price) {
+    return this->price=price;
+}
+float Headphones::get_price() const {
+    return price;
 }
 
-
-void Headphones::print_name_class() const {
-    cout<<"Headphones"<<endl;
+void Headphones::writeProduct() {
+    ofstream file("C:\\Users\\Tony\\CLionProjects\\Devices\\files\\Products.txt", ios::app);
+    string data=to_string(product_number)+","+name+","+ to_string(weight)+","+type_of_material+","+to_string(price)+",";
+    if(microphone){
+        data=data+"1";
+    }
+    else{
+        data+="0";
+    }
+    file<<data;
+    file.close();
 }
 
 void Headphones::print(std::ostream &os) const {
-    os<<"INFO: "<<endl
-      <<"Назва наушників: "<<name
-      <<"\nВага наушників: "<<weight<<" кг"
-      <<"\nТип наушників наушників: "<<type_of_material
-      <<"\nНаушники з мікрофоном: "<<microphone<<endl;
+    os<<endl<<"---Товар: Наушники---"<<endl
+      <<"Номер товару: "<<product_number<<endl
+      <<"Назва: "<<name<<endl
+      <<"Вага: "<<weight<<endl
+      <<"Матеріал: "<<type_of_material<<endl;
+    if(microphone){os<<"Наявність мікрофона: +"<<endl;}
+    else{os<<"Наявність мікрофона: -"<<endl;}
+    os<<"Ціна: "<<price<<endl;
 }
 
-void Headphones::print_class_name() const {
-    cout<<"Class name: Headphones"<<endl;
+void Headphones::readData(std::istream &is) {
+    cout<<"Введіть номер товару: ";
+    is >> product_number;
+    cout<<"Введіть назву наушників: ";
+    is >> name;
+    cout<<"Введіть вагу наушників: ";
+    is >> weight;
+    cout<<"Введіть матеріал наушників: ";
+    is >> type_of_material;
+    cout << "Введіть чи є мікрофон (1-так, 0-ні): ";
+    is >> microphone;
+    cout<<"Введіть ціну наушників: ";
+    is >> price;
 }
 
-Headphones::Headphones(std::string &&NameOfKeyboard, float Weight, std::string &&TypeOfMaterial, bool MICROPHONE):
-        name{NameOfKeyboard}, weight(Weight), type_of_material(TypeOfMaterial), microphone(MICROPHONE)
-{
-    cout<<"called Headphones constructor"<<endl;
-}
 Headphones::Headphones(const Headphones &&other) noexcept{
+    product_number=other.product_number;
     name=move(other.name);
     weight=other.weight;
     type_of_material=move(other.type_of_material);
     microphone=other.microphone;
+    price=other.price;
     cout<<"Headphones move constructor"<<endl;
 }
+
+Headphones::Headphones(int product_number, std::string &&name, float weight, std::string &&type_of_material,
+                       float price, bool microphone):
+        product_number{product_number},name{name}, weight{weight}, type_of_material{type_of_material}
+        ,microphone{microphone},price{price}
+{
+    //cout<<"called Headphones constructor"<<endl;
+}
+
 Headphones::~Headphones(){
-    cout<<"called Headphones destructor"<<endl;
+    //cout<<"called Headphones destructor"<<endl;
 }
--- Headphones.h
+++ Headphones.h
@@ -1,3 +1,7 @@
+//
+// Created by Endi on 27.02.2024.
+//
+
 #ifndef DEVICESOOP_HEADPHONES_H
 #define DEVICESOOP_HEADPHONES_H
 
@@ -8,28 +12,34 @@
 using namespace std;
 class Headphones: public Printable{
 private:
+    int product_number;
     string name;
     float weight;
     string type_of_material;
     bool microphone;
+    float price;
 
 public:
+    int set_product_number(int product_number);
+    int get_product_number()const;
     string set_name(string name);
-    string get_name();
+    string get_name()const;
     float set_weight(float weight);
-    float get_weight();
+    float get_weight()const;
     string set_TypeOfMaterial(string type_of_material);
-    string get_TypeOfMaterial();
+    string get_TypeOfMaterial()const;
     bool set_microphone(bool microphone);
-    bool get_microphone();
-    void info();
+    bool get_microphone()const;
+    float set_price(float price);
+    float get_price()const;
 
-    virtual void print_name_class()const;
     virtual void print(std::ostream &os) const override;
-    virtual void print_class_name()const override;
-
-    Headphones(string &&NameOfHeadphones="None", float Weight=0, string &&TypeOfMaterial="None", bool MICROPHONE=false);
+    virtual void readData(std::istream& is) override;
+    virtual void writeProduct() override;
     Headphones(const Headphones &&other)noexcept;
+
+    Headphones(int product_number=0,string &&name="None", float weight=0, string &&type_of_material="None"
+            ,float price=0, bool microphone= false);
     virtual ~Headphones();
 };
 
